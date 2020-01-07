e567725e152baee61beb16e379186fbbfc6e7e27
Jordan Yang <JYang@newera.local>
2020-01-07 14:31:15 -0500

    Updates

---
+++ precache-manifest.6e77ca1207388a4556e49b2044e582f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25a45e611a2b625f9c0c86d4495346d",
+    "url": "/JYprofile/index.html"
+  },
+  {
+    "revision": "b1e6de556a06bce02163",
+    "url": "/JYprofile/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "9ad4f4350b1857f46fe8",
+    "url": "/JYprofile/static/js/2.7b1e855e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/JYprofile/static/js/2.7b1e855e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1e6de556a06bce02163",
+    "url": "/JYprofile/static/js/main.94edd8cb.chunk.js"
+  },
+  {
+    "revision": "8575f14c5ee550cc1156",
+    "url": "/JYprofile/static/js/runtime-main.0a5bd9d7.js"
+  }
+]);
